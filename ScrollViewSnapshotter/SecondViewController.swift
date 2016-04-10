dd95a4fecab47cea1603e5d48558eb9ea0e301ca
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 10:16:33 -0400

    Initial Commit

---
+++ ScrollViewSnapshotter/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ScrollViewSnapshotter
+//
+//  Created by Moshe Berman on 4/10/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
