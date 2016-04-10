558dbe4d771b7798ce3d5b71beb1af906d525039
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 12:41:28 -0400

    Added a snapshotter class.

---
+++ ScrollViewSnapshotter/ScrollViewSnapshotter.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollViewSnapshotter.swift
+//  ScrollViewSnapshotter
+//
+//  Created by Moshe Berman on 4/10/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewSnapshotter: NSObject {
+
+}
