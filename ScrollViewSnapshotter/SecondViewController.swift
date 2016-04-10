0bbf691a0239a1c28b16f928ff41063bc7b56254
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 18:26:25 -0400

    Table view responds to notifications about data changes on disk.

--- ScrollViewSnapshotter/SecondViewController.swift
+++ ScrollViewSnapshotter/SecondViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UITableViewController {
 
+    /**
+     * A list of the saved files
+     */
+    var PDFPaths : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updatePaths), name: "com.mosheberman.pdf-saved", object: nil)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.updatePaths()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,39 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //  MARK: - UITableViewDataSource
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.PDFPaths.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        if indexPath.row < self.PDFPaths.count {
+            let index = self.PDFPaths.startIndex.advancedBy(indexPath.row)
+            let path = self.PDFPaths[index]
+            
+            cell.textLabel?.text = path.componentsSeparatedByString("/").last
+        }
+        
+        return cell
+    }
+    
+    // MARK: - Helpers
+    
+    /**
+     *  Here we update the paths by re-loading the contents of the directory.
+     */
+    
+    func updatePaths()
+    {
+        let manager = PDFManager()
+        
+        self.PDFPaths = manager.filesInDocumentsDirectory()
+    }
 }
 
