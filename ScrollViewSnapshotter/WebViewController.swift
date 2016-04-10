4de152ae80c56ba6b076fbb3d7748af45e1d97cf
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 18:40:25 -0400

    Added a Web View for showing PDF.

---
+++ ScrollViewSnapshotter/WebViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WebViewController.swift
+//  ScrollViewSnapshotter
+//
+//  Created by Moshe Berman on 4/10/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    
+    @IBOutlet weak let webView : UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
