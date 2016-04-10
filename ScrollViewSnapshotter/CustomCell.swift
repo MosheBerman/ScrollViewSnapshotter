261160eb4f1cea1ab6a5851cf01236b5c1c28181
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 11:09:14 -0400

    Set up a collection view to demonstrate a scroll view that extends beyond the scope of the view.

---
+++ ScrollViewSnapshotter/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  ScrollViewSnapshotter
+//
+//  Created by Moshe Berman on 4/10/16.
+//  Copyright © 2016 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
--- ScrollViewSnapshotter/FirstViewController.swift
+++ ScrollViewSnapshotter/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UICollectionViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,38 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! CustomCell
+        
+        let images = ["first", "second"]
+        
+        if let label = cell.textLabel
+        {
+            label.text = "IndexPath: \(indexPath.section),\(indexPath.row)"
+        }
+        
+        if let imageView = cell.imageView
+        {
+            let n = Int(arc4random()) % Int(images.count)
+            let imageIndex = images.startIndex.advancedBy(n)
+            let imageName = images[imageIndex]
+            let image = UIImage(named: imageName)
+            imageView.image = image
+        }
+        
+        return cell
+    }
 
 }
 
