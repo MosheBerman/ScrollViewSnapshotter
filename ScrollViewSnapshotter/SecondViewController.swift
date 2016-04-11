b826476ea1ab2286ebb3d16aa0e0df36c0ccb92a
Moshe Berman <moshefromsinai@gmail.com>
2016-04-10 20:32:49 -0400

    Show document previews.

--- ScrollViewSnapshotter/SecondViewController.swift
+++ ScrollViewSnapshotter/SecondViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class SecondViewController: UITableViewController {
-
+class SecondViewController: UITableViewController, UIDocumentInteractionControllerDelegate {
+    
     /**
      * A list of the saved files
      */
@@ -19,20 +19,20 @@ class SecondViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updatePaths), name: "com.mosheberman.pdf-saved", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(updatePaths(_:)), name: "com.mosheberman.pdf-saved", object: nil)
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.updatePaths()
+        self.updatePaths(nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     //  MARK: - UITableViewDataSource
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
@@ -55,17 +55,51 @@ class SecondViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if indexPath.row < self.PDFPaths.count {
+            
+            
+            let path = self.PDFPaths[self.PDFPaths.startIndex.advancedBy(indexPath.row)]
+            
+            let manager = PDFManager()
+            let fullPath = "\(manager.documentsDirectory())/\(path)"
+            
+            let url = NSURL(fileURLWithPath: fullPath)
+            
+            let interactionController = UIDocumentInteractionController(URL: url)
+            interactionController.delegate = self
+            
+            if interactionController.presentPreviewAnimated(true) {
+                print("Presented preview.")
+            }
+            else
+            {
+                print("Did not present preview.")
+            }
+        }
+        
+    }
+    
+    // MARK: -
+    
+    func documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+    
     // MARK: - Helpers
     
     /**
      *  Here we update the paths by re-loading the contents of the directory.
      */
     
-    func updatePaths()
+    func updatePaths(notification : NSNotification?)
     {
         let manager = PDFManager()
         
         self.PDFPaths = manager.filesInDocumentsDirectory()
+        
+        self.tableView.reloadData()
     }
 }
 
